f10d9171d90931636b8f704ed85026954c9a2c95
clairespills <purpleprincess372@gmail.com>
2018-07-24 15:53:19 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,10 +1,10 @@
-def breakfast(morning_snacc="Frosted Flakes")
+def breakfast(morning_snacc="Frosted Flakes").downcase)
   puts "Morning is the best time for #{morning_snacc}!"
 end
-def lunch(noon_snacc="Grilled Cheese")
+def lunch(noon_snacc="Grilled Cheese").downcase)
   puts "Noon is the best time for #{noon_snacc}!"
 end
-def dinner(evenining_snacc="Salmon")
+def dinner(evenining_snacc="Salmon").downcase)
   puts "Evening is the best time for #{evenining_snacc}!"
 end
 
